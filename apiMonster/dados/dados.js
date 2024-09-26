893701ce8b8b06b42a894ca302e73f1b8d841368
AnaCarolina16 <cordeiro.caroline.ana@estudante.ifro.edu.br>
2024-09-26 11:44:30 -0400

    ...

---
+++ apiMonster/dados/dados.js
@@ -0,0 +1,43 @@
+const colecaomonster = [
+    {
+        id: 1,
+        nome:'Monster Energy Original'
+    },
+    {
+        id: 2,
+        nome: "Monster Energy Absolutely Zero"
+    },
+    {
+        id: 3,
+        nome: "Monster Energy Ultra Paradise"
+    },
+    {
+        id: 4,
+        nome: "Monster Energy Zero Ultra"
+    },
+    {
+        id: 5,
+        nome: "Monster Energy Ultra Violet"
+    },
+    {
+        id: 6,
+        nome: "Monster Energy Ultra Watermelon"
+    },
+    {
+        id: 7,
+        nome: "Monster Energy Juice Khaos"
+    },
+    {
+        id: 8,
+        uf: "ES",
+        nome: "Espírito Santo"
+    },
+    {
+        id: 9,
+        nome: "Monster Energy Juice Pacific Punch"
+    },
+    {
+        id: 10,
+        nome: "Monster Energy Juice Mango Loco"
+    }]
+    exports.colecaomonster = colecaomonster;
---
+++ apiMonster/index.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const colecaomonster = express('./dados/dados' );
+
+const app = colecaomonster
