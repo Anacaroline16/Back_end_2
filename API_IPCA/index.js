6a71277d270e42ea40286dea2a7e3796e7c8f7ab
anacaroline16.github.io <caroline.cordeiro@estudante.ifro.edu.br>
2024-11-24 18:49:30 -0300

    atualizaçao 24/11

--- API_IPCA/index.js
+++ API_IPCA/index.js
@@ -1,5 +1,5 @@
 import  express  from "express";
-import {buscar_td_Dados_ipca, buscar_ipcaPorAno, buscar_ipca_porId} from  './servicos/servico.js'
+import {buscar_td_Dados_ipca, buscar_ipcaPorAno, buscar_ipca_porId, valor_reajustado} from  './servicos/servico.js'
 
 const app = express();
 
@@ -9,19 +9,43 @@ app.get('/historicoIPCA', (req, res) => {
     if (resultado.length > 0) {
         res.json(resultado);
     } else {
-        res.status(404).send({"erro": "Nenhum ano encontrado"});
+        res.status(404).send({"erro": "Nenhum historico encontrado para o ano especificado."});
     }
 });
 app.get('/historicoIPCA/:id', (req,res) => {
-    const uf = buscar_ipca_porId(req.params.id);
-    if(uf){
-        res.json(uf);
+    const id = buscar_ipca_porId(req.params.id);
+    if(id){
+        res.json(id);
     } else if (isNaN(parseInt(req.params.id))){
         res.status(400).send({'erro' : 'Requisição inválida'});
     } else {
-        res.status(404).send({'erro': 'UF não encontrada'});
+        res.status(404).send({'erro': 'Elemento não encontrado.'});
     }
 });
+
+app.get('/historicoIPCA/calculo', (req, res) => {
+    const {valor , mesInicial, anoInicial, mesFinal, anoFinal} = req.query;
+    
+    if (!valor || !mesInicial || !anoInicial || !mesFinal ||!anoFinal){
+        res.status(400).json({'erro': 'Parâmentros inválidos.'});
+    }
+    try{
+        const resultado = valor_reajustado(
+            parseFloat(valor),
+            parseInt(mesInicial),
+            parseInt(anoInicial),
+            parseInt(mesFinal),
+            parseInt(anoFinal)
+        );
+        res.json({valor_reajustado: resultado});
+    } catch (error){
+        res.status(400).json({erro: error.message});
+    }
+    
+    
+});
+
+
 app.listen(8080,() => {
     console.log('Servidor iniciado na porta 8080');
 });
--- API_IPCA/servicos/servico.js
+++ API_IPCA/servicos/servico.js
@@ -4,9 +4,38 @@ export const buscar_td_Dados_ipca = () => {
     return historicoInflacao;
 }
 export const buscar_ipcaPorAno = (ano) => {
-    return historicoInflacao.filter(ipca => ipca.ano == ano);
+    return historicoInflacao.filter((ipca) => ipca.ano == parseInt(ano));
 }
 export const buscar_ipca_porId = (id) => {
     const idipca = parseInt(id);
-    return historicoInflacao.find(ipca => ipca.id === idipca);
+    return historicoInflacao.find((ipca) => ipca.id === idipca);
 }
+
+export const valor_reajustado = (valor , mesInicial , anoInicial, mesFinal, anoFinal ) => {
+
+    
+    if (!valor || valor <= 0){
+        throw new Error('O valor inicial deve ser maior que 0.');
+    }
+    if (anoInicial> anoFinal || (anoInicial===anoFinal && mesInicial> mesFinal)){
+        throw new Error('O intervalo de datas é inválido .');
+    }
+
+    const dadosFiltrados = historicoInflacao.filter((ipca) => {
+        return(
+            (ipca.ano >  anoInicial && ipca.ano < anoFinal) ||
+            (ipca.ano === anoInicial && ipca.mes >= mesInicial) ||
+            (ipca.ano === anoFinal && ipca.mes <= mesFinal) 
+        );
+    });
+
+    if (dadosFiltrados.length === 0){
+       throw new Error('Nenhum índice encontrado para os dados fornecido.');
+    };
+    let resultado = valor;
+    dadosFiltrados.forEach((ipca) => {
+        resultado *= 1 + ipca.valor / 100;
+    });
+    
+    return resultado;
+};
