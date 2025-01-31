fa02987513b5a8f56d324c0086096498788e1f4e
AnaCarolina16 <cordeiro.caroline.ana@estudante.ifro.edu.br>
2025-01-31 11:43:43 -0400

    atualizacao 31/01

--- api_libertadores2/servicos/retornaCampeonatos_servico.js
+++ api_libertadores2/servicos/retornaCampeonatos_servico.js
@@ -5,14 +5,28 @@ export async function retornaCampeonatos() {
     const campeonatos_tb = await conexao.query('Select id, campeao, vice, ano FROM campeonatos');
     const campeonatos = campeonatos_tb[0];
     conexao.release();
-    return campeonatos;
-    
+    return campeonatos; 
 }
 
 export async function retornaCampeonatosID(id){
     const conexao = await pool.getConnection();
-    const campeonatos_tb = await conexao.query
-    ('Select id, campeao, vice, ano FROM campeonatos WHERE id =' +id);
+    const campeonatos_tb = await conexao.query('Select id, campeao, vice, ano FROM campeonatos WHERE id =' +id);
+    const campeonatos = campeonatos_tb[0];
+    conexao.release();
+    return campeonatos;
+}
+
+export async function retornaCampeonatosAno(ano){
+    const conexao = await pool.getConnection();
+    const campeonatos_tb = await conexao.query('Select id, campeao, vice, ano FROM campeonatos WHERE ano =' + ano);
+    const campeonatos = campeonatos_tb[0];
+    conexao.release();
+    return campeonatos;
+}
+
+export async function retornaCampeonatosTime(time){
+    const conexao = await pool.getConnection();
+    const campeonatos_tb = await conexao.query('Select id, campeao, vice, ano FROM campeonatos WHERE campeao = " ' + time + ' " ');
     const campeonatos = campeonatos_tb[0];
     conexao.release();
     return campeonatos;
